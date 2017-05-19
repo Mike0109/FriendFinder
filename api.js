93706395471aed401980436bc059ee6c6b9bb221
Mike0109 <Mike0109@github.com>
2017-05-18 20:51:58 -0700

    done I think

---
+++ api.js
@@ -0,0 +1,43 @@
+var friendData 		= require('../data/friends.js');
+var path 			= require('path');
+
+// API GET Requests - when users "visit" a page. 
+// (ex:localhost:PORT/api/admin...they are shown a JSON of the data in the table) 
+
+var totalDifference = 0;
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+
+//API POST Request-handles when user submits a form & thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate Javascript array
+
+
+	app.post('/api/friends', function(req, res){
+
+		var greatMatch = {
+			name: "",
+			image: "",
+			matchDifference: 1000
+		};
+		var usrData 	= req.body;
+		var usrName 	= usrData.name;
+		var usrImage 	= usrData.image;
+		var usrScores 	= usrData.scores;
+
+		var totalDifference = 0;
+
+		//loop through the friends data array of objects to get each friends scores
+		for(var i = 0; i < [friends].length-1; i++){
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			//wut
+			
+			}
+		}
+
+		
